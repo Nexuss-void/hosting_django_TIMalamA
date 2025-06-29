af9d929cf30c611738a36adc91c4d4d42b352871
Nexuss-void <channicholas8@gmail.com>
2025-06-29 19:40:34 +0700

    update

--- layananhotel/layananhotel/settings.py
+++ layananhotel/layananhotel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'layananhotel_app',
     #'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 #Customize model user
